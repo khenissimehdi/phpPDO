88ca768adeef4e5517938473840b6b38cc6c2a9b
Baptiste DUCLOS <baptiste.duclos@etudiant.univ-reims.fr>
2019-05-10 17:40:28 +0200

    Exercie Artiste2

--- Artist.class.php
+++ Artist.class.php
@@ -3,8 +3,30 @@ require_once("Entity.class.php");
 
 class Artist extends Entity
 {
-    public static function createFromId()
+    public static function createFromId(int $id):self
+    {
+        $stmt = MyPDO::getInstance()->prepare(<<<SQL
+            SELECT name, id
+            FROM artist
+            where id = :id
+SQL
+);
+        $stmt->setFetchMode (MyPDO::FETCH_CLASS, 'Artist');
 
-    getAll();
+        $stmt->execute([':id' => $id]);
+
+
+        $artist = $stmt->fetch();
+
+        if ($artist === false) {
+            // lancer une exception
+        }
+        return $artist;
+    }
+
+    public static function getAll():void
+    {
+
+    }
 
 }
